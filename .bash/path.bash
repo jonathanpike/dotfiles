ff89b0f2be797b7e4246f703eeb19a433abc735c
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-02-13 12:52:35 -0500

    Add plan9 to path

--- .bash/path.bash
+++ .bash/path.bash
@@ -3,3 +3,6 @@
 export PATH=/usr/local/bin:$PATH
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+export PATH=$PATH:$PLAN9/bin 
+
+PLAN9=/usr/local/plan9 export PLAN9

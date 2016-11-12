a5bfa08031f0b71e849ff1815d56c6f4327e2c79
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-11-12 13:19:07 -0500

    Change PS1 to show user@host with full directory

--- .bash/theme.bash
+++ .bash/theme.bash
@@ -4,16 +4,16 @@
 PS1=''
 
 # Put in current directory and time only
-PS1+='\[$BLUE\]\T \W'
+PS1+='\[$BLUE\]\T \u@\h: \w'
 
 # If we have __git_ps1 installed, then put it in the prompt. We do what we can
 # from the previous two lines.
 if command -v __git_ps1 > /dev/null 2>&1; then
-  PS1+='\[$YELLOW\]$(__git_ps1 " (%s)")'
+  PS1+='\[$YELLOW\]$(__git_ps1 " (%s)")\n'
 fi
 
 # Show unstaged (*) and staged (+) changes 
 export GIT_PS1_SHOWDIRTYSTATE=1 
 
 # Normalize prompt contents
-PS1+='\[${NORMAL}\] $ '
+PS1+='\[${NORMAL}\]$ '

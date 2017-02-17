7f12462b08745df46da689ecb1b2ed0af82d3675
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2017-02-16 20:31:11 -0500

    Remove Plan9 from path

--- .bash/path.bash
+++ .bash/path.bash
@@ -6,5 +6,3 @@ export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 export PATH=$PATH:$PLAN9/bin 
 export GOPATH=$HOME/Code/Go
 export PATH=$PATH:$GOPATH/bin
-
-PLAN9=/usr/local/plan9 export PLAN9
